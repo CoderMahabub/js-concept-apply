9e45847c299a905e6bcc6f1bfe9016a83445b902
CoderMahabub <codermahabub@gmail.com>
2021-08-04 12:56:43 +0600

    IsOddNumber and isEvenNumber

---
+++ oddEven.js
@@ -0,0 +1,28 @@
+// const number = 4;
+// const reminder = 5;
+// console.log(reminder%2 == 0);
+
+// Is Even
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+const myNumber = 145;
+const isMyNumberEven = isEven(myNumber);
+console.log('Is number even? ', isMyNumberEven);
+
+const herNumber = 1892;
+const isHerNumberEven = isEven(herNumber);
+
+//Is Odd number
+function isOdd(number) {
+    if (number % 2 != 0) {
+        return true;
+    }
+    return false;
+}
+const dadNumber = 1243;
+const isDadNumberOdd = isOdd(dadNumber);
+console.log('Is dadNumber Odd? : ', isDadNumberOdd);
